c0a5ca0ec6ce07d4eb1115a951ab87f659d3ec4a
Prajwal Wani <119412705+Prajwalwani05@users.noreply.github.com>
2023-05-26 19:58:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function minDate(dates) {
   //write you code here
+	const parsedDates = dates.map(dateString => new Date(dateString));
+  const minDate = new Date(Math.min.apply(null, parsedDates));
+  return minDate.toISOString().slice(0, 10)
+	
 }
 
 // Do not change the code
@@ -38,3 +42,4 @@ var dates = [
 ];
 
 alert(minDate(dates));
+
